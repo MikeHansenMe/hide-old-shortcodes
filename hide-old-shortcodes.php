f76b88a53049f6160d25b2908c700d33faf48082
Mike Hansen <web.mhansen@gmail.com>
2013-12-10 10:59:28 -0700

    Add the Allow and Remove feature

--- hide-old-shortcodes.php
+++ hide-old-shortcodes.php
@@ -11,56 +11,57 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 function hos_return_all_shortcodes_regex() {
 	return
-		  '\\['                              // Opening bracket
-		. '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
-		. "([\w-]+)"                     	 // 2: Shortcode name
-		. '(?![\\w-])'                       // Not followed by word character or hyphen
-		. '('                                // 3: Unroll the loop: Inside the opening shortcode tag
-		.     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+		  '\\['									// Opening bracket
+		. '(\\[?)'								// 1: Optional second opening bracket for escaping shortcodes: [[tag]]
+		. "([\w-]+)"							// 2: Shortcode name
+		. '(?![\\w-])'							// Not followed by word character or hyphen
+		. '('									// 3: Unroll the loop: Inside the opening shortcode tag
+		.     '[^\\]\\/]*'						// Not a closing bracket or forward slash
 		.     '(?:'
-		.         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
-		.         '[^\\]\\/]*'               // Not a closing bracket or forward slash
+		.         '\\/(?!\\])'					// A forward slash not followed by a closing bracket
+		.         '[^\\]\\/]*'					// Not a closing bracket or forward slash
 		.     ')*?'
 		. ')'
 		. '(?:'
-		.     '(\\/)'                        // 4: Self closing tag ...
-		.     '\\]'                          // ... and closing bracket
+		.     '(\\/)'							// 4: Self closing tag ...
+		.     '\\]'								// ... and closing bracket
 		. '|'
-		.     '\\]'                          // Closing bracket
+		.     '\\]'								// Closing bracket
 		.     '(?:'
-		.         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
-		.             '[^\\[]*+'             // Not an opening bracket
+		.         '('							// 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+		.             '[^\\[]*+'				// Not an opening bracket
 		.             '(?:'
-		.                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
-		.                 '[^\\[]*+'         // Not an opening bracket
+		.                 '\\[(?!\\/\\2\\])'	// An opening bracket not followed by the closing shortcode tag
+		.                 '[^\\[]*+'			// Not an opening bracket
 		.             ')*+'
 		.         ')'
-		.         '\\[\\/\\2\\]'             // Closing shortcode tag
+		.         '\\[\\/\\2\\]'				// Closing shortcode tag
 		.     ')?'
 		. ')'
-		. '(\\]?)';                          // 6: Optional second closing bracket for escaping shortcodes: [[tag]]
+		. '(\\]?)';								// 6: Optional second closing bracket for escaping shortcodes: [[tag]]
 }
 
 function hos_replace_old_shortcodes( $content ) {
 	global $shortcode_tags;
 	global $post;
-	$post_type = get_post_type( $post ); 											//used later to help locate the usage
-	$pattern = hos_return_all_shortcodes_regex(); 									//returns our modified regex to return ALL shortcodes
+	$post_type = get_post_type( $post );											//used later to help locate the usage
+	$pattern = hos_return_all_shortcodes_regex();									//returns our modified regex to return ALL shortcodes
 	preg_match_all( '/' . $pattern . '/s', $content, $matches, PREG_SET_ORDER );
 	foreach ( $matches as $match ) {
 		$raw_match = $match;														//store the raw match to show usage
 		$match = $match[2];															//just the name of the shortcode
 		if( ! array_key_exists( $match, $shortcode_tags ) ) { 						//check that it is not a active shortcode
 			$hidden_shortcodes = get_option( 'hos_hidden_shortcodes', array() );
-			if( ! array_key_exists( 'hos_' . md5( $raw_match[0] . "-" . $post_type ), $hidden_shortcodes ) ) {	//check if we already logged this, if so save time and skip
-				$hidden_shortcodes[ 'hos_' . md5( $raw_match[0] . "-" . $post_type ) ] = array(
+			if( ! array_key_exists( 'hos_' . md5( $raw_match[0] . "-" . $post->ID ), $hidden_shortcodes ) AND strpos( $raw_match[0],'[[') === false ) {	//check if we already logged this, if so save time and skip
+				$hidden_shortcodes[ 'hos_' . md5( $raw_match[0] . "-" . $post->ID ) ] = array(
 					'shortcode' => $match,
 					'raw' => $raw_match[0],
-					'post_type' => $post_type
+					'post_type' => $post_type,
+					'post_id' => $post->ID
 					);
-				update_option( 'hos_hidden_shortcodes', $hidden_shortcodes ); 		//save usage for log
+				update_option( 'hos_hidden_shortcodes', $hidden_shortcodes );		//save usage for log
 			}
-			$shortcode_tags[ $match ] = '__return_false'; 							//make the shortcode do nothing(hide)
+			$shortcode_tags[ $match ] = '__return_false';							//make the shortcode do nothing(hide)
 		}
 	}
 	return $content;
@@ -68,25 +69,48 @@ function hos_replace_old_shortcodes( $content ) {
 add_filter( 'the_content', 'hos_replace_old_shortcodes' );
 
 function hos_add_page() {
-	add_management_page( 'Hide Old Shortcodes', 'Hide Old Shortcodes', 'publish_posts', 'hide-old-shortcodes', 'hos_page_content' );
+	add_management_page( 'Hide Old Shortcodes', 'Hide Old Shortcodes', 'edit_posts', 'hide-old-shortcodes', 'hos_page_content' );
 }
 add_action( 'admin_menu', 'hos_add_page' );
 
 function hos_page_content() {
-	if( isset( $_GET['clear'] ) AND $_GET['clear'] == true ){
+	$message = array();
+	if( isset( $_GET['clear'] ) AND $_GET['clear'] == true ) {
 		update_option( 'hos_hidden_shortcodes', array() );
 	}
+	
 	$hidden_shortcodes = get_option( 'hos_hidden_shortcodes', array() );
+	
+	if( isset( $_GET['action'] ) AND $_GET['action'] == 'display' AND isset( $hidden_shortcodes[ esc_attr( $_GET['key'] ) ] ) ) {
+		$display_shortcode = $hidden_shortcodes[ esc_attr( $_GET['key'] ) ];
+		hos_display_shortcode( $display_shortcode['post_id'], esc_attr( $_GET['key'] ), $display_shortcode['raw'] );
+		$message[] = array( 'type' => 'updated', 'message' => 'Shortcode ' . $display_shortcode['raw'] . ' will now show on the front.' );
+		unset( $hidden_shortcodes[ esc_attr( $_GET['key'] ) ] );
+	}
+
+	if( isset( $_GET['action'] ) AND $_GET['action'] == 'remove' AND isset( $hidden_shortcodes[ esc_attr( $_GET['key'] ) ] ) ) {
+		$remove_shortcode = $hidden_shortcodes[ esc_attr( $_GET['key'] ) ];
+		hos_remove_shortcode( $remove_shortcode['post_id'], esc_attr( $_GET['key'] ), $remove_shortcode['raw'] );
+		$message[] = array( 'type' => 'updated', 'message' => 'Shortcode "' . $remove_shortcode['raw'] . '" has been removed.' );
+		unset( $hidden_shortcodes[ esc_attr( $_GET['key'] ) ] );
+	}
 	?>
 	<div class="wrap">
 	<h2>Hide Old Shortcodes</h2>
-	<h4>Here is a list of shortcodes that have been blocked from being seen on the front end.</h4>
+	<h4>Here is a list of shortcodes that have been blocked from being viewed in the content.</h4>
+	<?php
+	if( count( $message ) > 0 ){
+		for ( $i=0;  $i < count( $message );  $i++ ) { 
+			echo "<div class='" . $message[ $i ]['type'] . "'><p>" . $message[ $i ]['message'] . "</p></div>";
+		}
+	}
+	?>
 	<table class="widefat">
 		<thead>
 			<tr>
 				<th>Shortcode</th>
 				<th>Raw Usage</th>
-				<th>Locate</th>					
+				<th>Actions</th>
 			</tr>
 		</thead>
 	<?php
@@ -94,7 +118,11 @@ function hos_page_content() {
 		echo "<tr>
 				<td>" . $hidden_shortcode_v['shortcode'] . "</td>
 				<td>" . $hidden_shortcode_v['raw'] . "</td>
-				<td><a href='edit.php?s=" . $hidden_shortcode_v['shortcode'] . "&post_type=" . $hidden_shortcode_v['post_type'] . "'>" . $hidden_shortcode_v['post_type'] . "</a></td>
+				<td>
+					<a href='post.php?post=" . $hidden_shortcode_v['post_id'] . "&action=edit'>Edit " . $hidden_shortcode_v['post_type'] . "</a> |
+					<a href='tools.php?page=hide-old-shortcodes&action=display&key=" . $hidden_shortcode_k . "'>Allow</a> |
+					<a href='tools.php?page=hide-old-shortcodes&action=remove&key=" . $hidden_shortcode_k . "'>Remove</a>
+				</td>
 			</tr>";
 	}
 	?>
@@ -113,4 +141,22 @@ function hos_page_content() {
 	</div>
 	</div>
 	<?php
+}
+
+function hos_display_shortcode( $id, $key, $raw ) {
+	$post = get_post( $id, ARRAY_A );
+	$post['post_content'] = str_replace( $raw, '[' . $raw . ']', $post['post_content'] );
+	wp_update_post( $post );
+	$shortcode_log = get_option( 'hos_hidden_shortcodes', array() );
+	unset( $shortcode_log[ $key ] );
+	update_option( 'hos_hidden_shortcodes', $shortcode_log );
+}
+
+function hos_remove_shortcode( $id, $key, $raw ) {
+	$post = get_post( $id, ARRAY_A );
+	$post['post_content'] = str_replace( $raw, '', $post['post_content'] );
+	wp_update_post( $post );
+	$shortcode_log = get_option( 'hos_hidden_shortcodes', array() );
+	unset( $shortcode_log[ $key ] );
+	update_option( 'hos_hidden_shortcodes', $shortcode_log );
 }
